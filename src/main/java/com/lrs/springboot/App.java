f9cef1895d18883caa8811b36af1c294257a4cd0
Swedish-li <1993770815@qq.com>
2017-02-27 23:23:51 +0800

    ObjectMapper配置测试添加，WebMvc配置添加

--- src/main/java/com/lrs/springboot/App.java
+++ src/main/java/com/lrs/springboot/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * spring boot 示例项目
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @SpringBootApplication
+@EnableWebMvc
 public class App {
 	@RequestMapping("/")
 	public String index(ModelMap model) {
