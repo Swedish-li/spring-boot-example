450e37e7a9a8a5368105af324bb111603fae59da
Swedish-li <1993770815@qq.com>
2017-03-08 20:38:42 +0800

    添加banner文件

--- src/main/java/com/lrs/springboot/App.java
+++ src/main/java/com/lrs/springboot/App.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 在使用Maven搭建时出现启动项目后立即关闭的现象，原因为使用Maven下载的Tomcat集成jar有问题,删除重新下载就可以了 在启动参数中添加
  * --debug 参数可以查看Spring boot的启动信息
  * 
+ * http://www.network-science.de/ascii/
+ * http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+ * 
  * slf4j log4j logback关系详解和相关用法：http://www.cnblogs.com/Sinte-Beuve/p/5758971.html
  * 
  * @author Swedish-li
