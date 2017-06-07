7ef685d2f3a5721b4bdb3fa6a41f26600b026fb1
Swedish-li <one.piece.li.2016@gmail.com>
2017-06-07 17:45:34 +0800

    添加拦截器配置示例

--- src/main/java/com/lrs/springboot/config/WebMvcConfig.java
+++ src/main/java/com/lrs/springboot/config/WebMvcConfig.java
@@ -1,8 +1,24 @@
 package com.lrs.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.lrs.springboot.config.interceptor.RequestInterceptor;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new RequestInterceptor()).addPathPatterns("/**");
+		super.addInterceptors(registry);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("static/**");
+	}
 
 }
---
+++ src/main/java/com/lrs/springboot/config/interceptor/RequestInterceptor.java
@@ -0,0 +1,39 @@
+package com.lrs.springboot.config.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class RequestInterceptor implements HandlerInterceptor {
+
+	private final static Logger logger = LoggerFactory.getLogger(RequestInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String url = request.getRequestURI();
+		System.out.println();
+		logger.info("请求开始,url:{},handler:{}", url, handler);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		String url = request.getRequestURI();
+		logger.info("控制器处理完成-{}", url);
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		logger.info("请求数据渲染完成-{}", request.getRequestURI());
+
+	}
+
+}
