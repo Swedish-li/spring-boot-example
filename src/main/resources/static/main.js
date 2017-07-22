fd48c9df5e1097f6c24a4b7e6b28ac95c5ee560c
Swedish-li <one.piece.li.2016@gmail.com>
2017-07-22 16:18:20 +0800

    代码格式化，依赖修改

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,11 +1,12 @@
+/* global requirejs */
 const currentVersion = new Date().getTime()
 
 requirejs.config({
-	urlArgs: "v=" + currentVersion,
-	baseUrl : "/lib",
-	paths : {
-		"echarts" : "echarts"
-	}
+  urlArgs: 'v=' + currentVersion,
+  baseUrl: '/lib',
+  paths: {
+    'echarts': '//cdn.bootcss.com/echarts/3.6.2/echarts.min'
+  }
 })
 
 requirejs(['../js/app'])
