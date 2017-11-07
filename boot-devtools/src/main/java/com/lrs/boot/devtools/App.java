4b44c6398d476cc18c62b5e3b56ab7f7d738e458
Swedish-li <one.piece.li.2016@gmail.com>
2017-11-07 11:26:16 +0800

    add spring-boot-devtools example

---
+++ boot-devtools/src/main/java/com/lrs/boot/devtools/App.java
@@ -0,0 +1,23 @@
+package com.lrs.boot.devtools;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+	@GetMapping("index")
+	public String index(HttpServletRequest req) {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
